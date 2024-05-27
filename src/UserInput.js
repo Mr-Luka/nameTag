ae88e1f90d8a26f87f993719ede973f698ce157a
Luka <luka.luka.luka93@gmail.com>
2024-05-27 10:34:24 -0700

    default export of User Input

--- src/UserInput.js
+++ src/UserInput.js
@@ -9,4 +9,19 @@ class UserInput extends Component {
         e.preventDefault();
         this.setState({name: ""});
     }
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="Add a new name here..."
+                  value={this.state.name}
+                  onChange={this.updateName}
+                />
+                <input type="submit" value="Create Name Tag"/>
+            </form>
+        );
+    }
 }
+
+export default UserInput;
