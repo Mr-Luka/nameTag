a6525206ebf464f57bd245e20853934dee84a9d5
Luka <luka.luka.luka93@gmail.com>
2024-05-29 16:31:46 -0700

    empty array

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UserInput from "./UserInput.js";
 
 class App extends Component {
   state = {
-    names: ["Erin", "Ann", "Nichole", "Sharon", "Maryn"]
+    names: []
   };
   removeName = (clickedIndex) => {
     
